8ecd5917aadd67fefbf2244821ac8c32d7cba2a5
QBUTEAU <buteauquentin10@gmail.com>
2025-02-10 11:23:32 +0000

    feat: php tests correction

--- src/Controller/BuggyController.php
+++ src/Controller/BuggyController.php
@@ -12,11 +12,11 @@ class BuggyController extends AbstractController
     public function buggyAction(): Response
     {
         $data = $this->getData();
-
+        $message = $data['nonexistent_key'] ?? 'Je ne bug plus !';
         return $this->render('welcome.html.twig', [
-            'data' => $data['nonexistent_key'], // Bug : accès à une clé inexistante
-        ]);
-    }
+            'message' => $message,
+    ]);
+}
 
     private function getData(): array
     {
